a721111d7b8afbb6d1f59f717362c60794ffd7c4
lucasbaldezzari <lmbaldezzari@gmail.com>
2025-08-02 10:29:40 -0300

    Se actualiza

--- funciones/tests.py
+++ funciones/tests.py
@@ -1,2 +1,42 @@
-def hola_mundo():
-    print("¡Hola, mundo!")
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import StandardScaler
+#importo kmeans
+from sklearn.cluster import KMeans
+#importo dbscan
+from sklearn.cluster import DBSCAN
+#importo para poder crear data moons y data circles
+from sklearn.datasets import make_moons, make_circles
+
+def transform_and_get_iris():
+    """
+    Columna original	Nombre simulado	            Descripción de negocio
+    sepal length (cm)	mdeia_visitas_diarias	    Cantidad promedio de visitas diarias al producto (popularidad)
+    sepal width (cm)	precio_unitario	            Precio en USD del producto
+    petal length (cm)	unidades_vendidas_mensual	Unidades vendidas por mes
+    petal width (cm)	valoracion_media	        Valoración media de usuarios (1 a 5)
+
+    labels = Tipo de cliente que más compra ese producto
+
+    Código orig	       Descripción del cliente	        ¿Qué busca?
+    0	               Cliente explorador	            Navega mucho, compra poco. Interesado en novedades
+                                                        y precios bajos.	Buen precio, variedad, promociones
+    1	               Cliente comprometido	            Compra de forma recurrente productos específicos.
+                                                        Fiel a ciertos productos.	Alta valoración, confianza, calidad
+    2	               Cliente impulsivo	            Compra mucho y rápido. Le importa menos el precio si
+                                                        el producto es atractivo.	Ventas altas, buena valoración
+        
+    """
+    iris = load_iris()
+    ##cambio las columnas
+    df = pd.DataFrame(iris.data, columns=
+                      [
+                          "mdeia_visitas_diarias",
+                          "precio_unitario",
+                          "unidades_vendidas_mensuales",
+                          "valoración_media"
+                      ])
+    df["segmento"] = iris.target
+
+    return df
